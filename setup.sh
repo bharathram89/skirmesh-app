b956170c530896c005eb43c325526ba235d90208
Brandon <brandonzoss@gmail.com>
2020-12-19 07:13:21 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-cd ~/Projects/battlefield
+echo "Navigating to home directory - creating ~/Coding/battlefield"
+cd ~/
+mkdir Coding
+cd Coding
+mkdir /battlefield
+
+echo "Navigating to ~/Coding/battlefield"
+cd ~/Coding/battlefield
 
 sudo apt-get update -y
 sudo apt-get upgrade -y
@@ -14,3 +21,27 @@ python3 -m venv ./venv
 
 venv/bin/pip3 install --upgrade setuptools
 venv/bin/pip3 install -r requirements.txt
+
+echo "Generating service file at /etc/systemd/system/battlefield.service"
+sudo cat > /etc/systemd/system/battlefield.service <<EOF
+[Unit]
+Description=Battlefield Init Service
+
+[Service]
+WorkingDirectory=/home/pi/Coding/battlefield/
+ExecStart=/home/pi/Coding/battlefield/venv/bin/python main.py
+StandardOutput=inherit
+StandardError=inherit
+Restart=on-failure
+RestartSec=15
+
+[Install]
+WantedBy=multi-user.target
+Alias=battlefield.service
+EOF
+
+echo "enabling /etc/systemd/system/battlefield.service"
+sudo systemctl enable battlefield.service
+sudo systemctl enable battlefield.service
+
+echo "Battlefield Gaming Systems is fully enabled."
