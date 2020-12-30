31cbaf695b8b9aeaf5628a1dd90e5ed904bbb106
Dustin Kuchenbecker <dustin.kuchenbecker@gmail.com>
2020-12-29 21:09:22 -0500

    uid call is working

--- main.py
+++ main.py
@@ -258,7 +258,7 @@ def comms_log():
     return render_template('comms.html', **kwargs)
 
 @application.route('/user_reg', methods=['POST', 'GET'])
-def user_reg(uid=None):
+def user_reg():
 
     form = RegistrationForm(request.form)
 
@@ -280,12 +280,16 @@ def user_reg(uid=None):
 
         SQL.add_row(conn, 'player', data)
 
+        conn.close()
+
+        return redirect(url_for('user_reg'))
+
     conn.close()
 
     return render_template('user_reg.html',
                            form=form,
                            Players=players,
-                           uid=uid)
+                          )
 
 @application.route('/user_reg/get_uid', methods=['POST','GET'])
 def get_uid():
@@ -298,7 +302,7 @@ def get_uid():
 
     return make_response(jsonify({"message": "OK", "uid": uid}), 200)
 
-@application.route('/user_reg/assign_uid', methods=['POST', 'GET'])
+@application.route('/user_reg/assign_uid', methods=['POST'])
 def assign_uid():
 
     data = json.loads(request.data)
