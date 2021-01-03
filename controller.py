704b171c68bf5a606816921b11cf2db2d112a8e2
Brandon <brandonzoss@gmail.com>
2021-01-03 09:15:24 -0500

    update team color to TAN for 9 Jan event

--- controller.py
+++ controller.py
@@ -51,7 +51,7 @@ class CONTROL_POINT(XBeeDevice):
     # Define teams by color, but this could change...
     TEAM_CMAP = {RED    : '#FF0000',
                  BLUE   : '#0000FF',
-                 YELLOW : 'D2B48C',      #'#FFFF00',
+                 YELLOW : '#D2B48C',      #'#FFFF00',
                  GREEN  : '#008000',
                  PURPLE : '#3333CC'}
 
