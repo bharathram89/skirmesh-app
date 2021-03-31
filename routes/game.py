3db73719bc3398bc33460c754dd3b41700721857
Brandon Zoss <brandonzoss@gmail.com>
2021-03-30 21:11:00 -0400

    add route for medic ability

--- routes/game.py
+++ routes/game.py
@@ -1,5 +1,5 @@
 from database import db_session
-from models.db_models import RFID, TeamPlayer
+from models.db_models import RFID, TeamPlayer, Device
 
 from flask import jsonify, request, make_response
 from flask import Blueprint
@@ -45,5 +45,67 @@ def register_team():
             rfid.teamPlayer.is_alive  = True
 
         db_session.commit()
+
+    return None
+
+
+
+@bp.route('/gameplay/check_medic', methods=['PUT'])
+def check_medic():
+
+    """
+    API to handle medic action.
+
+    PUT  - update medic status
+
+    :: args    ::       uid, address
+
+    :: returns ::       medic status  {'alive':bool,'lights':int}
+    """
+
+    if request.method == 'PUT':
+
+        params = request.json
+
+        uid    = params.pop('uid', None)
+        node   = params.pop('address', None)
+
+        if not uid or not node: return None
+
+        rfid = RFID.query.filter(RFID.uid == uid).first()
+        node = Device.query.filter(Device.address == node).first()
+
+        if not rfid or not node: return None
+
+        ALL      = 5
+        med_time = node.med_time * 10
+        d_t      = datetime.utcnow() - rfid.teamPlayer.lastChange
+
+        if not rfid.teamPlayer.is_alive and d_t.total_seconds() >= med_time:
+            # If he was dead and has waited the correct amount of time,
+            # bring him back to life
+            rfid.teamPlayer.is_alive = True
+            db_session.commit()
+
+            return jsonify({'alive':True,'lights':ALL})
+        # If he's DEAD and not enough time has passed to bring him back
+        # to life, then let him knows how close he is to being alive.
+        if not rfid.teamPlayer.is_alive and d_t.total_seconds() < med_time:
+
+            time_rem = med_time - d_t.total_seconds()
+            time_rem = min(max(0, time_rem), med_time)
+
+            num_lights = int(5 * (time_rem/med_time))
+
+            return jsonify({'alive':False,'lights':num_lights})
+
+
+        if rfid.teamPlayer.is_alive:
+            # If he was alive, and he's at the medic, he probably died...
+            rfid.teamPlayer.is_alive = False
+            db_session.commit()
+
+            return jsonify({'alive':False,'lights':ALL})
+
 
     return None
