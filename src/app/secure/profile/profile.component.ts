07fd9462f79eb9f80ce531d5caa3b6348437a4bc
Brandon Zoss <brandonzoss@gmail.com>
2021-04-08 20:49:08 -0400

    correct default assignment to integer

--- src/app/secure/profile/profile.component.ts
+++ src/app/secure/profile/profile.component.ts
@@ -101,13 +101,13 @@ export class ProfileComponent implements OnInit {
           this.currentVals.profile = userData.fieldProfiles[0].profile ? userData.fieldProfiles[0].profile : 'Describe your Field!';
           this.currentVals.fieldName = userData.callSign ? userData.callSign : 'Your Field Name';
           this.currentVals.fieldProfileID = userData.fieldProfiles[0].id;
-          this.currentVals.imageID = userData.fieldProfiles[0].imageID ? userData.fieldProfiles[0].imageID : null;
+          this.currentVals.imageID = userData.fieldProfiles[0].imageID ? userData.fieldProfiles[0].imageID : 0;
         }
         else if (this.isPlayer) {
           this.currentVals.bio = userData.playerProfile.outfit ? userData.playerProfile.outfit : 'Tell us about your loadout!';
           this.currentVals.clanTag = userData.playerProfile.clanTag ? userData.playerProfile.clanTag : 'Declare your Clan!';
           this.currentVals.callSign = userData.playerProfile.callSign ? userData.callSign : 'Whats your callsign!';
-          this.currentVals.imageID = userData.playerProfile.imageID ? userData.playerProfile.imageID : null;
+          this.currentVals.imageID = userData.playerProfile.imageID ? userData.playerProfile.imageID : 0;
         }
         this.currentVals.firstName = userData.firstName ? userData.firstName : 'First Name';
         this.currentVals.lastName = userData.lastName ? userData.lastName : 'Last Name';
@@ -216,6 +216,7 @@ export class ProfileComponent implements OnInit {
                           'fieldProfileID':this.currentVals.fieldProfileID},
                  'image':{'data':this.base64toUpload}};
 
+    console.log(data)
 
     this.authSvc.saveImage(this.userSvc.getToken(), data);
 
