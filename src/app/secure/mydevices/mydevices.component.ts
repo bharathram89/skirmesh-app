c4cff36cc473eb9cac0975448e543a8cd21c978b
Bharadwaj Ramachandran <zbram@Bharadwajs-MacBook-Pro.local>
2021-05-04 00:54:29 -0700

    wip for teams section

--- src/app/secure/mydevices/mydevices.component.ts
+++ src/app/secure/mydevices/mydevices.component.ts
@@ -21,7 +21,7 @@ export class MydevicesComponent implements OnInit {
    activeGames;
    activeGame = false;
    map;
-   teams;
+   teams = [];
    description;
    currentTab = 'map'
    tokenSvc: TokenStorageService;
@@ -62,11 +62,24 @@ export class MydevicesComponent implements OnInit {
             console.log(gameConfig, "selected COnfigs details")
             this.activeGame = true;
             this.gameConfig = gameConfig;
-            this.teams = gameConfig["teams"];
+            gameConfig["teams"].forEach(team => {
+               let teamObj = {id:team.id,name:team.name,color:'#'+team.color,players:[]}
+               team.teamPlayers.forEach(player => {
+                  teamObj.players.push({rfID:player.rfidID
+                                       ,is_alive:player.is_alive
+                                       ,lastAction:this.findLastAction(team.id,player.rfidID,team)
+                                       ,lastLocation:this.findLastLocation(team.id,player.rfidID,team)
+                                       ,totalPoints:this.findTotalPoints(team.id,player.rfidID,team)
+                                    })
+               });
+               this.teams.push(teamObj)
+            });
+            
             this.map = gameConfig['mapID'];
             this.description = gameConfig['description'];
             this.devices = this.findDevicesForGameID(gameConfigID);
-            console.log(this.devices,"finsl divs")
+            console.log(this.devices,this.teams,"finsl divs")
+
 
          }
       )
@@ -76,6 +89,35 @@ export class MydevicesComponent implements OnInit {
    }
    findDevicesForGameID(gameID){
       return this.activeGames.filter(ele=> ele.gameConfigID == gameID && ele.devices.length > 0)[0].devices;
+   }
+   findLastActionTime(teamID,rfID){
+      let teamInfo = this.teams.find(ele=>ele.id = teamID);
+      let gameActions = teamInfo.gameActions;
+      let userActions = gameActions.filter(action => action.rfidID= rfID);
+      console.log(userActions,"findLastActionTime")
+      return userActions[0].creationDate
+   }
+   findLastAction(teamID,rfID,team){ 
+      let gameActions = team.gameActions;
+      let userActions = gameActions.filter(action => action.rfidID= rfID);
+      console.log(userActions,"findLastAction")
+      return  userActions[0].actionID 
+   }
+
+   findTotalPoints(teamID,rfID,team){ 
+      let gameActions = team.gameActions;
+      let userActions = gameActions.filter(action => action.rfidID= rfID);
+      let totalPoints = 0;
+      userActions.filter(action => action.points ? totalPoints = totalPoints + action.points:totalPoints = totalPoints )
+      console.log(totalPoints,"findTotalPoints")
+      return  totalPoints
+   }
+
+   findLastLocation(teamID,rfID,team){ 
+      let gameActions = team.gameActions;
+      let userActions = gameActions.filter(action => action.rfidID= rfID);
+      console.log(userActions,"findLastLocation")
+      return  userActions[0].deviceID 
    }
    //TODO: WE NEED TO STORE THIS SVG IN DB AND PULL IT DOWN TO SET.
    ngAfterViewInit() {
