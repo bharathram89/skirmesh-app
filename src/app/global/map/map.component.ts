326aeb794b1b274922814feaa6beafd561de6aea
Bharadwaj Ramachandran <zbram@Bharadwajs-MacBook-Pro.local>
2021-05-05 23:04:47 -0700

    map blinking

--- src/app/global/map/map.component.ts
+++ src/app/global/map/map.component.ts
@@ -20,13 +20,12 @@ export class MapComponent implements OnInit {
    }
 
   ngOnInit(): void {
-    console.log("** DATA RECIEVED IN MAP COMPONENT *** mapid: ",this.mapID," mapData: ",this.mapData)
-    let activeGame = JSON.parse(this.tokenSvc.getGameInfo());
-    if (activeGame) { 
-       this.map = this.userSvc.findMapName(activeGame.mapID);   
-    } else {
+    console.log("** DATA RECIEVED IN MAP COMPONENT *** mapid: ",this.mapID," mapData: ",this.mapData);
+    // if (activeGame) { 
+       this.map = this.userSvc.findMapName(this.mapID);   
+    // } else {
 
-    }
+    // }
     
   }
   locationClick(locationID){
@@ -36,17 +35,20 @@ export class MapComponent implements OnInit {
   }
   locationState(locationID){
     let deviceConfigForLocation = this.mapData.find(ele => ele.location == locationID);
+    console.log(deviceConfigForLocation,"locationState")
     if(deviceConfigForLocation && deviceConfigForLocation.enabled)return true;
     return false; 
   }
   
   shouldLocationBlink(locationID){
     let deviceConfigForLocation = this.mapData.find(ele => ele.location == locationID);
+    console.log(deviceConfigForLocation,"shouldLocationBlink")
     if(deviceConfigForLocation && deviceConfigForLocation.stable) return true //if stable dont blink
     return false //if not stable blink
   }
   locationColor(locationID){
     let deviceConfigForLocation = this.mapData.find(ele => ele.location == locationID);
+    console.log(deviceConfigForLocation,"locationColor")
     if(deviceConfigForLocation && deviceConfigForLocation.team) return '#'+deviceConfigForLocation.team //if team set use that color
     return 'transparent'//default to transparent without any team set
   }
