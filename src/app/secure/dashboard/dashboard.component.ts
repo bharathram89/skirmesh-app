fac10a1ced74aeee4ab9748bf93ccdbc400d57f9
Brandon <brandonzoss@gmail.com>
2021-05-14 08:57:38 -0400

    update player profile stats

--- src/app/secure/dashboard/dashboard.component.ts
+++ src/app/secure/dashboard/dashboard.component.ts
@@ -6,6 +6,14 @@ import { DeviceService } from 'src/service/device.service';
 import { GameService } from 'src/service/game.service';
 import { UserServiceService } from 'src/service/user-service.service';
 
+// actionIDs associated with specific actions
+const MEDIC    = 11;
+const CAPTURE  = 2;
+const ASSIST   = 3;
+const BOMB_ARM = 8;
+const BOMB_DIF = 9;
+
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -16,7 +24,7 @@ export class DashboardComponent implements OnInit {
 
   @ViewChild(DatatableComponent) table: DatatableComponent;
   ColumnMode = ColumnMode;
-  rows = [ 
+  rows = [
   ];
 
   currentVals = {
@@ -38,62 +46,122 @@ export class DashboardComponent implements OnInit {
   columns = [{ prop: 'name' }, { name: 'Gender' }, { name: 'Company' }];
   isPlayer;
   isField;
+
+  actionList;
+
+  allActions;
+  totalCaptures = 0;
+  totalAssists  = 0;
+  totalMedics   = 0;
+  totalBombArm  = 0;
+  totalBombDis  = 0;
+  totalPoints   = 0;
+
+
   gameSvc:GameService;
   authSvc:AuthService;
   userSvc:UserServiceService;
-  constructor(gameService: GameService,authService:AuthService,userService: UserServiceService) { 
+  constructor(gameService: GameService,authService:AuthService,userService: UserServiceService) {
     this.gameSvc = gameService;
     this.authSvc = authService;
     this.userSvc = userService;
   }
 
-  ngOnInit(): void {
+    ngOnInit(): void {
 
-    this.isPlayer        = this.userSvc.isPlayer;
-    this.isField         = this.userSvc.isField;
+        this.isPlayer        = this.userSvc.isPlayer;
+        this.isField         = this.userSvc.isField;
 
-    combineLatest([this.gameSvc.getPastGames(),this.userSvc.getUserData()]).subscribe(([pastGames,userData])=>{
-      console.log(pastGames,'all game')  
-      pastGames
+        combineLatest([this.userSvc.getUserData(), this.gameSvc.getActions()]).subscribe( ([userData, actions]) => {
 
+            this.actionList = actions;
 
-      if (this.isField) {
-        this.currentVals.profile = userData.fieldProfile.profile ? userData.fieldProfile.profile : 'Describe your Field!';
-        this.currentVals.fieldName = userData.callSign ? userData.callSign : 'Your Field Name';
-        this.currentVals.fieldProfileID = userData.fieldProfile.id;
-        this.currentVals.imageID = userData.fieldProfile.imageID ? userData.fieldProfile.imageID : 0;
-      }
-      else if (this.isPlayer) {
-        this.currentVals.bio = userData.playerProfile.outfit ? userData.playerProfile.outfit : 'Tell us about your loadout!';
-        this.currentVals.clanTag = userData.playerProfile.clanTag ? userData.playerProfile.clanTag : 'Declare your Clan!';
-        this.currentVals.callSign = userData.playerProfile.callSign ? userData.callSign : 'Whats your callsign!';
-        this.currentVals.imageID = userData.playerProfile.imageID ? userData.playerProfile.imageID : 0;
-      }
-      this.currentVals.firstName = userData.firstName ? userData.firstName : 'First Name';
-      this.currentVals.lastName = userData.lastName ? userData.lastName : 'Last Name';
-      this.currentVals.email = userData.email ? userData.email : 'E-mail';
-      this.currentVals.phone = userData.phoneNumber ? userData.phoneNumber : 'Phone Number';
+            this.setGameStats(userData);
 
-      this.currentVals.userID = userData.id;
+            console.log("PLAYER.TEAMPLAYER.ID", userData)
 
-    this.authSvc.getImage(this.currentVals.imageID).subscribe(
-      imageData => {
-        this.currentVals.imageData = imageData['image'] ? imageData['image'] : null;
-      }
-    )
-    })
-  }
-  updateFilter(event) {
-    const val = event.target.value.toLowerCase();
+            if (this.isField) {
+                this.currentVals.profile = userData.fieldProfile.profile ? userData.fieldProfile.profile : 'Describe your Field!';
+                this.currentVals.fieldName = userData.callSign ? userData.callSign : 'Your Field Name';
+                this.currentVals.fieldProfileID = userData.fieldProfile.id;
+                this.currentVals.imageID = userData.fieldProfile.imageID ? userData.fieldProfile.imageID : 0;
+            }
+            else if (this.isPlayer) {
+                this.currentVals.bio = userData.playerProfile.outfit ? userData.playerProfile.outfit : 'Tell us about your loadout!';
+                this.currentVals.clanTag = userData.playerProfile.clanTag ? userData.playerProfile.clanTag : 'Declare your Clan!';
+                this.currentVals.callSign = userData.playerProfile.callSign ? userData.callSign : 'Whats your callsign!';
+                this.currentVals.imageID = userData.playerProfile.imageID ? userData.playerProfile.imageID : 0;
+            }
+            this.currentVals.firstName = userData.firstName ? userData.firstName : 'First Name';
+            this.currentVals.lastName = userData.lastName ? userData.lastName : 'Last Name';
+            this.currentVals.email = userData.email ? userData.email : 'E-mail';
+            this.currentVals.phone = userData.phoneNumber ? userData.phoneNumber : 'Phone Number';
 
-    // filter our data
-    const temp = this.temp.filter(function (d) {
-      return d.name.toLowerCase().indexOf(val) !== -1 || !val;
-    });
+            this.currentVals.userID = userData.id;
 
-    // update the rows
-    this.rows = temp;
-    // Whenever the filter changes, always go back to the first page
-    this.table.offset = 0;
-  }
+            this.authSvc.getImage(this.currentVals.imageID).subscribe(
+                imageData => {
+                    this.currentVals.imageData = imageData['image'] ? imageData['image'] : null;
+                }
+            )
+        })
+    }
+
+    updateFilter(event) {
+
+        const val = event.target.value.toLowerCase();
+
+        // filter our data
+        const temp = this.temp.filter(function (d) {
+        return d.name.toLowerCase().indexOf(val) !== -1 || !val;
+        });
+
+        // update the rows
+        this.rows = temp;
+        // Whenever the filter changes, always go back to the first page
+        this.table.offset = 0;
+    }
+
+    setGameStats(userData){
+
+        let actions = [];
+
+        for (let rfid of userData.rfids) {
+            for (let action of rfid.gameActions) {
+                actions.push(action)
+            }
+        }
+
+        this.allActions = actions.sort((a, b) => b.id - a.id)
+
+        for (let action of actions) {
+            this.totalPoints += this.actionList.find(ele => ele.id == action.actionID).points;
+
+            switch(action.actionID) {
+
+                case MEDIC:
+                    this.totalMedics++;
+                    break;
+
+                case CAPTURE:
+                    this.totalCaptures++;
+                    break;
+
+                case ASSIST:
+                    this.totalAssists++;
+                    break;
+
+                case BOMB_ARM:
+                    this.totalBombArm++;
+                    break;
+
+                case BOMB_DIF:
+                    this.totalBombDis++;
+                    break;
+            }
+
+        }
+
+        console.log("TOTALS:  medic", this.totalMedics, "capture", this.totalCaptures)
+    }
 }
