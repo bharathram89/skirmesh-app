ab845702737901cdabe8a64a359aff36a0149344
Bharadwaj Ramachandran <zbram@Bharadwajs-MacBook-Pro.local>
2021-05-21 21:13:08 -0700

    styling in sign in and prod build

--- src/service/node-status.service.ts
+++ src/service/node-status.service.ts
@@ -1,39 +1,12 @@
 
-import { Injectable } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
-import { map }from 'rxjs/operators';
-import { Socket } from 'ngx-socket-io';
-import { environment } from '../environments/environment';
+import { Injectable } from '@angular/core';  
 
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { tokenName } from '@angular/compiler';
 import { TokenStorageService } from './token-storage.service';
 import { UserServiceService } from './user-service.service';
 
-
-@Injectable({
-  providedIn: 'root'
-})
-export class NodeConfigSocket extends Socket {
-
-  constructor() {
-    super({ url: environment.ws_url, options: {} });
-  }
-
-  public sendMessage(message) {
-    this.emit('new-message', message);
-  }
-
-  public getMessages = () => {
-    return new Observable((observer) => {
-            this.on('new-message', (message) => {
-                observer.next(message);
-            });
-    });
-  }
-
-
-}
+ 
 
 
 @Injectable({
