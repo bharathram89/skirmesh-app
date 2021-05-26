77f791a04fd3cf4546b0b5620fc7b6a76be4a7d8
Brandon <brandonzoss@gmail.com>
2021-05-26 15:47:30 -0400

    finish non-secure route setup

--- src/service/non-secure-api.service.ts
+++ src/service/non-secure-api.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -6,16 +7,20 @@ import { Injectable } from '@angular/core';
 export class NonSecureAPIService {
 
     // }
-    headers =new HttpHeaders({
+    headers = new HttpHeaders({
         'Content-Type': 'application/json'
     });
-    options = { headers: this.headers };
+
+    options = { headers : this.headers };
 
 
     BASE = 'https://api.skirmesh.net/'
 
     // setup source routes
     RSRC = 'resources/'
+    GAME = 'games/'
+    STAT = 'statistics/'
+    LOGN = 'login'
 
     // setup subroutes
     DEVC = 'devices'
@@ -36,7 +41,83 @@ export class NonSecureAPIService {
     ) {}
 
 
+    // █░░ █▀▀█ █▀▀▀ ░▀░ █▀▀▄ 　 █▀▀ █▀▀ █▀▀█ ▀█░█▀ ░▀░ █▀▀ █▀▀ █▀▀
+    // █░░ █░░█ █░▀█ ▀█▀ █░░█ 　 ▀▀█ █▀▀ █▄▄▀ ░█▄█░ ▀█▀ █░░ █▀▀ ▀▀█
+    // ▀▀▀ ▀▀▀▀ ▀▀▀▀ ▀▀▀ ▀░░▀ 　 ▀▀▀ ▀▀▀ ▀░▀▀ ░░▀░░ ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀
 
+
+    createUser(data){
+
+        let route = this.BASE + this.RSRC
+        return this.http.post(route + 'createUser',data,this.options)
+    }
+
+
+    userLogin(data){
+
+        return this.http.post(this.BASE + this.LOGN, data, this.options)
+    }
+
+    getImage(imageID) {
+
+        let route = this.BASE + this.RSRC + 'serve_image/' + imageID
+        return this.http.get(route)
+    }
+
+
+    // █▀▀▀ █▀▀█ █▀▄▀█ █▀▀ 　 ░▀░ █▀▀▄ █▀▀ █▀▀█ █▀▀█ █▀▄▀█ █▀▀█ ▀▀█▀▀ ░▀░ █▀▀█ █▀▀▄
+    // █░▀█ █▄▄█ █░▀░█ █▀▀ 　 ▀█▀ █░░█ █▀▀ █░░█ █▄▄▀ █░▀░█ █▄▄█ ░░█░░ ▀█▀ █░░█ █░░█
+    // ▀▀▀▀ ▀░░▀ ▀░░░▀ ▀▀▀ 　 ▀▀▀ ▀░░▀ ▀░░ ▀▀▀▀ ▀░▀▀ ▀░░░▀ ▀░░▀ ░░▀░░ ▀▀▀ ▀▀▀▀ ▀░░▀
+
+    getGameConfigsByID(gameConfigID){
+
+        let route = this.BASE + this.GCFG
+
+        const options = { params  : new HttpParams().set("id", gameConfigID) }
+
+        return this.http.get(route, options)
+    }
+    // This needs to get renamed to getActiveGames
+    getGames() {
+        return this.http.get(this.BASE + this.GAME + 'activeGames')
+    }
+
+    getPastGames() {
+        return this.http.get(this.BASE + this.GAME + 'pastGames')
+    }
+
+
+    getGameStats(gameID){
+
+        let route = this.BASE + this.STAT
+
+        const options = { params  : new HttpParams().set("id", gameID) }
+
+        return this.http.get(route + 'game', options)
+    }
+
+    getLocations(){
+        return this.http.get(this.BASE + this.RSRC + 'locations')
+    }
+
+    getMapData(mapID){
+
+        const options = { params  : new HttpParams().set("id", mapID) }
+
+        return this.http.get(this.BASE + this.RSRC + 'map', options)
+    }
+
+    getActions(){
+        return this.http.get(this.BASE + this.RSRC + 'actions')
+    }
+
+
+    getDevicesByGameID(gameID){
+
+        const options = { params  : new HttpParams().set("gameID", gameID) }
+
+        return this.http.get(this.BASE + this.RSRC + this.DEVC, options)
+    }
 
 
 }
