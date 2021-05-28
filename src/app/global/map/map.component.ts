bfade5a79ee4397db305406c8c214b593a3929b3
Brandon <brandonzoss@gmail.com>
2021-05-28 07:28:54 -0400

    shift map component to nonSecureAPI route

--- src/app/global/map/map.component.ts
+++ src/app/global/map/map.component.ts
@@ -2,8 +2,7 @@ import { Component, Input, OnInit, ɵConsole } from '@angular/core';
 import { Observable } from 'rxjs';
 import { GameService } from 'src/service/game.service';
 // import * as bootstrap from 'bootstrap';
-import { TokenStorageService } from 'src/service/token-storage.service';
-import { UserServiceService } from 'src/service/user-service.service';
+import { NonSecureAPIService } from 'src/service/non-secure-api.service';
 
 const REGISTER = 0x01;
 const QUERY = 0x02;
@@ -13,130 +12,127 @@ const MEDIC = 0x0E;
 const BOMB = 0xBB;
 
 @Component({
-  selector: 'app-map',
-  templateUrl: './map.component.svg',
-  styleUrls: ['./map.component.scss']
+    selector    : 'app-map',
+    templateUrl : './map.component.svg',
+    styleUrls   : ['./map.component.scss']
 })
 export class MapComponent implements OnInit {
 
-  @Input() mapID
-  @Input() deviceData
-  tooltipContent = ' '
-  socketOBJ;
-  map;
-  locationList;
-  mapData;
+    @Input() mapID
+    @Input() deviceData
 
-  tokenSvc: TokenStorageService
-  userSvc: UserServiceService;
-  gameSvc: GameService;
+    tooltipContent = ' '
+    socketOBJ;
+    map;
+    locationList;
+    mapData;
 
-  constructor(
-      tokenService: TokenStorageService,
-      userService: UserServiceService,
-      gameService: GameService
-  ) {
-    this.userSvc = userService;
-    this.tokenSvc = tokenService;
-    this.gameSvc = gameService;
-  }
+    gameSvc      : GameService;
+    nonSecAPIsvc : NonSecureAPIService;
 
-  ngOnInit(): void {
+    constructor(
+        gameService      : GameService,
+        nonSecAPIservice : NonSecureAPIService
+    ) {
+        this.gameSvc = gameService;
+        this.nonSecAPIsvc = nonSecAPIservice;
+    }
 
-    this.socketOBJ = this.gameSvc.getDeviceUpdate().subscribe(socketData => {
-      // console.log("** DATA RECIEVED IN MAP COMPONENT *** mapid: ",this.mapID," deviceData: ",this.deviceData)
-      console.log(socketData, " Device Update");
-      this.updateLocationState(socketData);
-    })
+    ngOnInit(): void {
 
-  }
+        this.socketOBJ = this.gameSvc.getDeviceUpdate().subscribe(socketData => {
+            console.log(socketData, "Device Update");
+            this.updateLocationState(socketData);
+        })
 
-  ngOnDestroy(): void {
-    //Called once, before the instance is destroyed.
-    //Add 'implements OnDestroy' to the class.
-    this.socketOBJ.unsubscribe()
-  }
+    }
 
-  ngAfterViewInit(): void {
-    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
-    //Add 'implements AfterViewInit' to the class.
-    this.updateMapState()
+    ngOnDestroy(): void {
+        //Called once, before the instance is destroyed.
+        //Add 'implements OnDestroy' to the class.
+        this.socketOBJ.unsubscribe()
+    }
 
-    this.gameSvc.getMapData(this.mapID).subscribe(
+    ngAfterViewInit(): void {
+    //Called after ngAfterContentInit when the component's view has been initialized.
+    //Applies to components only.
+        this.updateMapState()
 
-        mapData => {
+        this.nonSecAPIsvc.getMapData(this.mapID).subscribe(
 
-            this.mapData = mapData;
-            this.locationList = this.mapData.locations;
+            mapData => {
 
-            let paths = document.getElementsByClassName("location");
+                this.mapData = mapData;
+                this.locationList = this.mapData.locations;
 
-            for (let i = 0; i < paths.length; i++) {
+                let paths = document.getElementsByClassName("location");
 
-                paths[i].addEventListener("mouseenter", event => {
+                for (let i = 0; i < paths.length; i++) {
 
-                    let target = event.target as HTMLTextAreaElement;
+                    paths[i].addEventListener("mouseenter", event => {
 
-                    let loc = this.locationList.find(ele => ele.id == target.id.replace('loc',''));
-                    this.tooltipContent = loc ? loc.name : "Mystery Zone";
+                        let target = event.target as HTMLTextAreaElement;
 
-                });
+                        let loc = this.locationList.find(ele => ele.id == target.id.replace('loc',''));
+                        this.tooltipContent = loc ? loc.name : "Mystery Zone";
+
+                    });
+                }
+
             }
+        )
 
+
+    }
+
+
+    updateMapState() {
+        for (let device of this.deviceData) { this.updateLocationState(device) }
+    }
+
+
+    updateLocationState(device) {
+
+        let locID = device.location;
+        let stable = device.stable;
+        let color = '#' + device.team;
+
+        let element = document.getElementById("loc" + locID);
+        // If the device corresponds to a location and the configuration
+        // is of CAPTURE or REGISTER ... maybe later ... MEDIC, update
+        // to beacon or fill the color as appropriate
+        if (element && (device.config == CAPTURE || device.config == REGISTER)) {
+
+            if (!stable && device.team) {
+
+                element.classList.remove("owned");
+                element.classList.add("beacon");
+                element.setAttribute("fill", color);
+
+            }
+            else if (stable && device.team) {
+
+                element.classList.remove("beacon");
+                element.classList.add("owned");
+                element.setAttribute("fill", color);
+            }
+            else {
+
+                element.classList.remove("owned");
+                element.classList.remove("beacon");
+                element.setAttribute("fill", null);
+            }
         }
-    )
+        // If we found an element, but the device is not one of the above,
+        // then give it standard styling.
+        else if (element) {
 
+            element.classList.remove("owned");
+            element.classList.remove("beacon");
+            element.setAttribute("fill", null);
+        }
 
-  }
-
-
-  updateMapState() {
-    for (let device of this.deviceData) { this.updateLocationState(device) }
-  }
-
-
-  updateLocationState(device) {
-
-    let locID = device.location;
-    let stable = device.stable;
-    let color = '#' + device.team;
-
-    let element = document.getElementById("loc" + locID);
-    // If the device corresponds to a location and the configuration
-    // is of CAPTURE or REGISTER ... maybe later ... MEDIC, update
-    // to beacon or fill the color as appropriate
-    if (element && (device.config == CAPTURE || device.config == REGISTER)) {
-
-      if (!stable && device.team) {
-
-        element.classList.remove("owned");
-        element.classList.add("beacon");
-        element.setAttribute("fill", color);
-
-      }
-      else if (stable && device.team) {
-
-        element.classList.remove("beacon");
-        element.classList.add("owned");
-        element.setAttribute("fill", color);
-      }
-      else {
-
-        element.classList.remove("owned");
-        element.classList.remove("beacon");
-        element.setAttribute("fill", null);
-      }
     }
-    // If we found an element, but the device is not one of the above,
-    // then give it standard styling.
-    else if (element) {
-
-      element.classList.remove("owned");
-      element.classList.remove("beacon");
-      element.setAttribute("fill", null);
-    }
-
-  }
 
 
 
