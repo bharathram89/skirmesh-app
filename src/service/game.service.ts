0e5248052a6b2a9678f1994abff62851a3ec3ce8
Brandon Zoss <brandonzoss@gmail.com>
2021-05-29 12:43:24 -0400

    cleanup game service

--- src/service/game.service.ts
+++ src/service/game.service.ts
@@ -1,4 +1,3 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Socket } from 'ngx-socket-io';
 import { Observable } from 'rxjs';
@@ -42,54 +41,5 @@ export class GameService {
               });
       });
     }
-
-    // getGamesByFieldProfile(token, fieldProfileID) {
-    //     return this.http.get(this.BASE + 'resources/gamesBy?token=' + token + "&fieldProfileID=" + fieldProfileID)
-    // }
-
-    // getActiveGamesByFieldProfile(token, fieldProfileID) {
-    //     return this.http.get(this.BASE + 'games/activeGamesBy?token=' + token + "&fieldProfileID=" + fieldProfileID)
-    // }
-
-    // getGames() {
-    //     return this.http.get(this.BASE + this.GTK)
-    // }
-    //
-    // getPastGames() {
-    //     return this.http.get(this.BASE + '/games/pastGames')
-    // }
-
-    // pauseGame(token, data) {
-    //     return this.http.put(this.BASE + 'gameplay/pauseGame?token=' + token, data, this.options)
-    // }
-
-    // startGame(token, gameConfigID){
-    //     let data = {"gameConfigID":gameConfigID}
-    //     return this.http.post(this.BASE+'games/startGame?token='+token,data,this.options )
-    // }
-
-    // endGame(token, gameID){
-    //     let data = {"id":gameID}
-    //     return this.http.put(this.BASE+'games/endGame?token='+token,data,this.options )
-    // }
-
-//     getGameStats(gameID){
-//         return this.http.get(this.BASE + 'statistics/game?id=' + gameID )
-//     }
-//
-//     getLocations(){
-//       return this.http.get(this.BASE + 'resources/locations')
-// //http://api.skirmesh.net/resources/locations
-//     }
-//
-//     getMapData(mapID){
-//       return this.http.get(this.BASE + 'resources/map?id=' + mapID)
-// //http://api.skirmesh.net/resources/locations
-//     }
-//
-//     getActions(){
-//       return this.http.get(this.BASE + 'resources/actions')
-// //http://api.skirmesh.net/resources/locations
-//     }
 
 }
