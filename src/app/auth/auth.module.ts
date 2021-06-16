5ba21cf41ff5ca012dcd320a5ddaa52d41fb82d4
Bharadwaj Ramachandran <zbram@Bharadwajs-MacBook-Pro.local>
2021-06-16 15:12:54 -0700

    form fix

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -7,8 +7,9 @@ import { SignUpComponent } from './sign-up/sign-up.component';
 import { SignInComponent } from './sign-in/sign-in.component'; 
 import { GlobalModule } from '../global/global.module'; 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PassResetComponent } from './pass-reset/pass-reset.component'
 @NgModule({
-  declarations: [AuthComponent, SignUpComponent, SignInComponent],
+  declarations: [AuthComponent, SignUpComponent, SignInComponent, PassResetComponent],
   imports: [
     CommonModule,
     AuthRoutingModule,
