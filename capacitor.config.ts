ac62e23b4656a61df900fa38b0c01138cbfebc94
Dishant Sheth <dishantsheth13@gmail.com>
2021-08-09 01:42:34 +0530

    android and ios

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'skirmesh-ui',
+  webDir: 'dist/skirmesh-ui',
+  bundledWebRuntime: false
+};
+
+export default config;
